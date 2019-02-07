45e4e7db472cf1b63615755d94d5bee1a3733782
Yurii Zhuravlov <nabirator@gmail.com>
2019-02-08 01:59:50 +0200

    Generate CRUD

---
+++ src/Form/AnimalType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Animal;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnimalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+            ->add('name')
+            ->add('description')
+            ->add('age')
+            ->add('gender')
+            ->add('breed')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Animal::class,
+        ]);
+    }
+}
